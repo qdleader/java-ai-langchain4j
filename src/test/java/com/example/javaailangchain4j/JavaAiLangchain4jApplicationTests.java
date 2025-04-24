616514dde5d39f5dc1124c41d39b7b95bdd338e3
前端架构师 <62106726+qdleader@users.noreply.github.com>
2025-04-24 21:00:04 +0800

    init project

---
+++ src/test/java/com/example/javaailangchain4j/JavaAiLangchain4jApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.javaailangchain4j;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiLangchain4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("http://langchain4j.dev/demo/openai/v1")
+                .apiKey("demo")
+                .modelName("gpt-4o-mini")
+                .build();
+
+        String answer = model.chat("你好");
+        System.out.println(answer);
+    }
+
+}
