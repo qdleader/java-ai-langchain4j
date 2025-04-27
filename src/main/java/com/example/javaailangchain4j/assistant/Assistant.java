ae07291ee4b30fd0e206258e3161915653fa7817
前端架构师 <62106726+qdleader@users.noreply.github.com>
2025-04-27 08:17:32 +0800

    init project

---
+++ src/main/java/com/example/javaailangchain4j/assistant/Assistant.java
@@ -0,0 +1,9 @@
+package com.example.javaailangchain4j.assistant;
+
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+
+@AiService(wiringMode = AiServiceWiringMode.EXPLICIT,chatModel = "openAiChatModel")
+public interface Assistant {
+    String chat(String userMessage);
+}
--- src/test/java/com/example/javaailangchain4j/JavaAiLangchain4jApplicationTests.java
+++ src/test/java/com/example/javaailangchain4j/JavaAiLangchain4jApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.javaailangchain4j;
 
+import com.example.javaailangchain4j.assistant.Assistant;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.spring.AiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +29,22 @@ class JavaAiLangchain4jApplicationTests {
     @Test
     public void testSpringBoot() {
        String answer =  openAiChatModel.chat("你是谁");
+       System.out.println(answer);
+    }
+
+    @Test
+    public void testInterface() {
+       Assistant assistant = AiServices.create(Assistant.class,openAiChatModel);
+       String answer = assistant.chat("你是谁");
+       System.out.println(answer);
+    }
+
+    @Autowired
+    private Assistant assistant;
+
+    @Test
+    public void testInterface2() {
+       String answer = assistant.chat("你是谁");
        System.out.println(answer);
     }
 
