037f3092c53360fcc9e64ed2398b3b279a0c41af
前端架构师 <62106726+qdleader@users.noreply.github.com>
2025-05-06 08:54:59 +0800

    添加系统消息支持及优化聊天隔离

--- src/main/java/com/example/javaailangchain4j/assistant/SeparateChatAssistant.java
+++ src/main/java/com/example/javaailangchain4j/assistant/SeparateChatAssistant.java
@@ -1,11 +1,13 @@
 package com.example.javaailangchain4j.assistant;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
 @AiService(wiringMode = AiServiceWiringMode.EXPLICIT,chatModel = "openAiChatModel",chatMemoryProvider = "chatMemoryProvider")
 public interface SeparateChatAssistant {
+    @SystemMessage("你是我的好朋友，请用东北话回答问题")
     String chat(@MemoryId int memoryId,@UserMessage String userMessage);
 }
