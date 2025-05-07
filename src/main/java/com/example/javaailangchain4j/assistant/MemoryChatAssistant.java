f787f95b91530dcf93ac65a3a1e823eafc152dca
前端架构师 <62106726+qdleader@users.noreply.github.com>
2025-05-07 08:36:47 +0800

    优化聊天接口，支持广东话及表情符号

--- src/main/java/com/example/javaailangchain4j/assistant/MemoryChatAssistant.java
+++ src/main/java/com/example/javaailangchain4j/assistant/MemoryChatAssistant.java
@@ -1,9 +1,12 @@
 package com.example.javaailangchain4j.assistant;
 
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
 @AiService(wiringMode = AiServiceWiringMode.EXPLICIT,chatModel = "openAiChatModel",chatMemory = "chatMemory")
 public interface MemoryChatAssistant {
-    String chat(String message);
+    @UserMessage("不是我的好朋友，请用广东话回答问题，并且添加一些表情符号。{{message}}")
+    String chat(@V("message") String message);
 }
