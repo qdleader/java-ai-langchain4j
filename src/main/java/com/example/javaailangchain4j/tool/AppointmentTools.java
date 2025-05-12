12eb153f7c475312cc81e4c2111cf663b7e86bc7
yangkun <yangkun@icourt.cc>
2025-05-12 13:54:28 +0800

    新增 Function calling

--- src/main/java/com/example/javaailangchain4j/tool/AppointmentTools.java
+++ src/main/java/com/example/javaailangchain4j/tool/AppointmentTools.java
@@ -13,7 +13,8 @@ public class AppointmentTools {
     @Autowired
     private AppointmentService appointmentService;
 
-    @Tool(name = "预约医生",value = "根据参数，先执行工具方法queryDepartment查询是否可预约，并直接给用户回答是否可预约，并让用户确认所有预约信息，用户确认后再进行预约。")
+    @Tool
+//    @Tool(name = "预约医生",value = "根据参数，先执行工具方法queryDepartment查询是否可预约，并直接给用户回答是否可预约，并让用户确认所有预约信息，用户确认后再进行预约。")
     public String bookAppointment(Appointment appointment) {
         System.out.println("预约医生"+appointment);
         Appointment appointmentDB = appointmentService.getOne(appointment);
@@ -28,7 +29,8 @@ public class AppointmentTools {
         return "您在相同的时间已经预约过该科室的医生了";
     }
 
-    @Tool(name = "取消预约挂号",value = "根据参数，查询预约是否存在，如果存在则删除预约记录并返回取消预约成功，否则返回取消预约失败。")
+//    @Tool(name = "取消预约挂号",value = "根据参数，查询预约是否存在，如果存在则删除预约记录并返回取消预约成功，否则返回取消预约失败。")
+   @Tool
     public String cancelAppointment(Appointment appointment) {
         Appointment appointmentDB = appointmentService.getOne(appointment);
         if (appointmentDB != null) {
@@ -41,7 +43,8 @@ public class AppointmentTools {
         return "取消预约失败，您没有预约该科室的医生";
     }
 
-    @Tool(name = "查询是否有号源",value = "根据科室名称，日期，时间和医生查询是否有号源，并返回给用户")
+//    @Tool(name = "查询是否有号源",value = "根据科室名称，日期，时间和医生查询是否有号源，并返回给用户")
+    @Tool
     public boolean queryDepartment(@P(value = "科室名称") String department, @P(value = "日期") String date, @P(value = "时间，可选值") String time, @P(value = "医生名称",required = false) String doctorName) {
         System.out.println("查询是否有号源"+department + date + time + doctorName);
         System.out.println("查询是否有号源");
